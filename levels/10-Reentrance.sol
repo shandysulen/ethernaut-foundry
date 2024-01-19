afa89eb8fc15ecbe03bd8b49a7eeb6d57bff5d42
Shandy Sulen <shandysulen@gmail.com>
2024-01-19 14:40:56 -0500

    install old and new versions of @openzeppelin/contracts

--- levels/10-Reentrance.sol
+++ levels/10-Reentrance.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 
-import 'openzeppelin-contracts/math/SafeMath.sol';
+import 'openzeppelin-contracts-solc-0.7/math/SafeMath.sol';
 
 contract Reentrance {
   
